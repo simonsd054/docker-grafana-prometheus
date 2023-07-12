11de4438aae0336af28e478b532ae492c03608f1
Simon Dahal <simonsd054@gmail.com>
2023-07-12 21:31:29 +1000

    Initial setup

---
+++ README.md
@@ -0,0 +1,12 @@
+# Introduction
+This is a testing project for prometheus and grafana using docker.
+
+# What it does?
+It saves data into prometheus using pushgateway and displays the data in grafana.
+
+# How to run?
+1. Clone/Download this repo
+2. Run the script `main.sh`
+    ```bash
+    ./main.sh
+    ```
---
+++ main.py
@@ -0,0 +1,49 @@
+from prometheus_client import CollectorRegistry, Gauge, push_to_gateway, start_http_server
+
+projectLists = {"ProjectA": 102, "ProjectB": 10,
+                "ProjectC": 25, "ProjectD": 82, "ProjectE": 75}
+
+projectServiceList = {
+    "ProjectA": {"Services": {"Ec2": 10, "Ec2 Other": 20, "Amazon Simple Storage Service": 30, "Aws Key management service": 40}},
+    "ProjectB": {"Services": {"Ec2": 15, "Ec2 Other": 30, "Amazon Simple Storage Service": 35, "Aws Key management service": 50}},
+    "ProjectC": {"Services": {"Ec2": 20, "Ec2 Other": 40, "Amazon Simple Storage Service": 40, "Aws Key management service": 60}},
+    "ProjectD": {"Services": {"Ec2": 25, "Ec2 Other": 50, "Amazon Simple Storage Service": 45, "Aws Key management service": 70}},
+    "ProjectE": {"Services": {"Ec2": 30, "Ec2 Other": 60, "Amazon Simple Storage Service": 50, "Aws Key management service": 80}}
+}
+
+def getProjectServices(projectName):
+    projectRegistry = CollectorRegistry()
+    projectGauge = Gauge(f"{projectName}_Services_Cost", "XC3 Project Breakdown Spend Cost",
+                         labelnames=["project_spend_services",
+                                     "project_spend_cost"],
+                         registry=projectRegistry)
+    
+    if projectName in projectServiceList.keys():
+        services = projectServiceList[projectName]["Services"]
+        if services:
+            for service, cost in services.items():
+                projectGauge.labels(service, cost).set(cost)
+                push_to_gateway("http://localhost:9091", job="project_service", registry=projectRegistry)
+    print(projectGauge)
+
+registry = CollectorRegistry()
+
+start_http_server(9091, registry=registry)
+
+gauge = Gauge("Project_Spend_Cost", "XC3 Project Spend Cost",
+              labelnames=["project_spend_project", "project_spend_cost"],
+              registry=registry)
+
+projectNameList = list(projectLists.keys())
+for project in projectLists.items():
+    projectName = project[0]
+    projectCost = project[1]
+    gauge.labels(projectName, projectCost).set(projectCost)
+
+print(gauge)
+
+push_to_gateway("http://localhost:9091", job="pushgateway", registry=registry)
+
+# for projectName in projectNameList:
+#     getProjectServices(projectName)
+
